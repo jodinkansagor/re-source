bd7e32283f5d4616942a0e33cf30de0be087c44c
Tali Avni <tali@aristo7.com>
2019-10-23 15:37:06 -0700

    small fixes

--- test/example.test.js
+++ test/example.test.js
@@ -18,7 +18,7 @@ test('renders display page', function(assert) {
         neighborhood: 'SW',
         type: ['medical']
     };
-    const expected = '<li class=\"avel-gordly-center-for-healing\"><a></a><div class=\"heading-div\"><a href=\"www.ohsuhealth.com/counseling\">Avel Gordly Center for Healing</a><input type=\"checkbox\" class=\"checkbox\" value=\"avel-gordly-center-for-healing\"></div><div class=\"contact-div\"><p class=\"phone\">Phone: (503)494-4745</p></div><p class=\"description\">Outpatient psychiatry, drug and alcohol counseling and treatment, culturally specific services. Sliding scale fee</p><p class=\"hours\">8:30a.m.-5p.m. Mon.-Fri.</p><div class=\"directions-div\"><p class=\"transit\">Downtown Bus, MAX</p><p class=\"address\">621 SW Alder St., Ste. 520</p></div></li>';
+    const expected = '<li class="avel-gordly-center-for-healing"><a></a><div class="heading-div"><a href="www.ohsuhealth.com/counseling">Avel Gordly Center for Healing</a><input type="checkbox" class="checkbox" value="avel-gordly-center-for-healing"></div><div class="contact-div"><p class="phone">Phone: (503)494-4745</p></div><p class="description">Outpatient psychiatry, drug and alcohol counseling and treatment, culturally specific services. Sliding scale fee</p><p class="hours">8:30a.m.-5p.m. Mon.-Fri.</p><div class="directions-div"><p class="transit">Downtown Bus, MAX</p><p class="address">621 SW Alder St., Ste. 520</p></div></li>';
 
     const dom = renderDisplayPage(avelGordlyCenterForHealing);
     const html = dom.outerHTML;
@@ -39,14 +39,14 @@ test('renders favorites page', function(assert) {
         neighborhood: 'SW',
         type: ['medical']
     };
-    const expected = '<li class=\"avel-gordly-center-for-healing\"><div class=\"heading-div\"><a href=\"www.ohsuhealth.com/counseling\">Avel Gordly Center for Healing</a></div><p>(503)494-4745</p><p>621 SW Alder St., Ste. 520</p></li>';
+    const expected = '<li class="avel-gordly-center-for-healing"><div class="heading-div"><a href="www.ohsuhealth.com/counseling">Avel Gordly Center for Healing</a></div><p>(503)494-4745</p><p>621 SW Alder St., Ste. 520</p></li>';
 
     const dom = renderFavorites(avelGordlyCenterForHealing);
     const html = dom.outerHTML;
     
     assert.equal(html, expected);
 });
-const list = document.createElement('ul');
+
 
 
 test('find product by id returns null if not found', assert => {
